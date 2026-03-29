4c8f94f8f5506581689e5f85e2bd3d396c2669a2
kaodi <70914418+KingKO04@users.noreply.github.com>
2026-03-29 13:32:36 -0400

    added choice to select plot type
    either histogram (default) or boxplot

--- boxplot app.R
+++ boxplot app.R
@@ -6,13 +6,24 @@ count <- 200
 
 # user interface
 ui <- bootstrapPage(
+  selectInput(
+    "plot_type", 
+    "Plot Type", 
+    c("Histogram" = "hist", "Boxplot" = "boxplot")
+  ),
   numericInput('count', "Number of obs", count),
   plotOutput("plot")
 )
 
 # server
-server <- function(input, output)
-  output$plot <- renderPlot(boxplot(runif(input$count), col = "red"))
+server <- function(input, output) {
+  output$plot <- renderPlot(
+    ifelse(
+      input$plot_type == "hist",
+      hist(runif(input$count), col = "red"), 
+      boxplot(runif(input$count), col = "red"))
+  )
+}
 
 # launch app
 shinyApp(ui, server)
